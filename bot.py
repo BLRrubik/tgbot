ac4100ab8227e395844d3fe7d000d8f606ae5467
BLRrubik <60672771+BLRrubik@users.noreply.github.com>
2020-02-05 00:30:57 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+﻿import config
+import telebot
+from telebot import apihelper
+bot = telebot.TeleBot('1005124044:AAEJvfJHObTgm6b-JyWH3z2Czcdme94nqbE')
+apihelper.proxy = {'https':'http://51.158.99.51:8811'}
+
+@bot.message_handler(commands = ['start'])
+def send_message(message):
+    bot.send_message(message.chat.id,"hi")
+
+#@bot.message_handler(content_types = ["text"])
+#def repeat_message(message):
+#	bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types = ['text'])
+def send_message(message):
+    if message.text.lower() == "саня пидр?":
+        bot.send_message(message.chat.id, "yes")
+    else:
+        bot.send_message(message.chat.id, message.text)
+
+
+bot.polling()
+
---
+++ config.py
@@ -0,0 +1,1 @@
+token = '1005124044:AAEJvfJHObTgm6b-JyWH3z2Czcdme94nqbE'
